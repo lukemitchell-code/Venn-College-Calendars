25e5189cce4d53d56a513adfc17be4c09b095139
lukemitchell-code <mitch.luke74@gmail.com>
2025-05-19 15:00:32 -0400

    Add files via upload

---
+++ VennCollegeCalendars.py
@@ -0,0 +1,163 @@
+{
+    "Princeton": {
+        "fall": {
+        "start": "2025-09-02",
+        "end": "2025-12-04",
+        "breaks": [
+           {
+             "name": "Fall Recess",
+             "start": "2025-10-11",
+             "end": "2025-10-19"
+           } 
+           {
+            "name": "Thanksgiving Recess",
+             "start": "2025-11-25",
+             "end": "2025-11-30"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-01-26",
+        "end": "2026-05-14",
+        "breaks": [
+           {
+             "name": "Spring Recess",
+             "start": "2026-03-07",
+             "end": "2026-03-15"
+           } 
+        ]
+    }
+}
+"Massachusetts Institute of Technology": {
+        "fall": {
+        "start": "2025-09-03",
+        "end": "2025-12-10",
+        "breaks": [
+           {
+             "name": "Memorial Day",
+             "start": "2025-09-19",
+             "end": "2025-09-19"
+           } 
+           {
+            "name": "Indigenous Peoples' Day",
+             "start": "2025-10-13",
+             "end": "2025-10-13"
+           }
+           {
+            "name": "Veterans Day",
+             "start": "2025-11-10",
+             "end": "2025-11-11"
+           }
+           {
+            "name": "Thanksgiving",
+             "start": "2025-11-27",
+             "end": "2025-11-28"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-02-02",
+        "end": "2026-05-12",
+        "breaks": [
+           {
+             "name": "Presidents' Day",
+             "start": "2026-02-16",
+             "end": "2026-02-16"
+           } 
+           {
+             "name": "Spring Break",
+             "start": "2026-03-23",
+             "end": "2026-03-27"
+           } 
+           {
+             "name": "Patriots' Day",
+             "start": "2026-04-20",
+             "end": "2026-04-20"
+           } 
+        ]
+    }
+}
+"University of California--Los Angeles": {
+     "fall": {
+        "start": "2025-09-25",
+        "end": "2025-12-12",
+        "breaks": [
+           {
+            "name": "Veterans Day",
+             "start": "2025-11-11",
+             "end": "2025-11-11"
+           }
+           {
+            "name": "Thanksgiving",
+             "start": "2025-11-27",
+             "end": "2025-11-28"
+           }
+        ]
+    }
+    "winter": {
+        "start": "2026-01-05",
+        "end": "2026-03-20",
+        "breaks": [
+           {
+            "name": "MLK Day",
+             "start": "2026-01-19",
+             "end": "2026-01-19"
+           }
+           {
+            "name": "Presidents' Day",
+             "start": "2026-02-16",
+             "end": "2026-02-16"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-03-30",
+        "end": "2026-06-12",
+        "breaks": [
+           {
+             "name": "Memorial Day",
+             "start": "2026-05-25",
+             "end": "2026-05-25"
+           } 
+        ]
+    }
+}
+"Washington University in St. Louis": {
+        "fall": {
+        "start": "2025-08-25",
+        "end": "2025-12-10",
+        "breaks": [
+           {
+             "name": "Labor Day",
+             "start": "2025-09-01",
+             "end": "2025-09-01"
+           } 
+           {
+            "name": "Fall Break",
+             "start": "2025-10-04",
+             "end": "2025-10-07"
+           }
+           {
+            "name": "Thanksgiving",
+             "start": "2025-11-26",
+             "end": "2025-11-30"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-01-12",
+        "end": "2026-05-06",
+        "breaks": [
+           {
+             "name": "MLK Day",
+             "start": "2026-01-19",
+             "end": "2026-01-19"
+           } 
+           {
+             "name": "Spring Break",
+             "start": "2026-03-08",
+             "end": "2026-03-15"
+           } 
+        ]
+    }
+}
