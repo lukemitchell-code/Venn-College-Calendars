e07af4a226b9c49df0f49e59f2750a82927ed8e9
lukemitchell-code <mitch.luke74@gmail.com>
2025-05-27 14:33:27 -0400

    Update VennCollegeCalendars.py

--- VennCollegeCalendars.py
+++ VennCollegeCalendars.py
@@ -1,82 +1,4 @@
 {
-    "Princeton University": {
-        "fall": {
-        "start": "2025-09-02",
-        "end": "2025-12-04",
-        "breaks": [
-           {
-             "name": "Fall Recess",
-             "start": "2025-10-11",
-             "end": "2025-10-19"
-           } 
-           {
-            "name": "Thanksgiving Recess",
-             "start": "2025-11-25",
-             "end": "2025-11-30"
-           }
-        ]
-    }
-    "spring": {
-        "start": "2026-01-26",
-        "end": "2026-05-14",
-        "breaks": [
-           {
-             "name": "Spring Recess",
-             "start": "2026-03-07",
-             "end": "2026-03-15"
-           } 
-        ]
-    }
-}
-"Massachusetts Institute of Technology": {
-        "fall": {
-        "start": "2025-09-03",
-        "end": "2025-12-10",
-        "breaks": [
-           {
-             "name": "Memorial Day",
-             "start": "2025-09-19",
-             "end": "2025-09-19"
-           } 
-           {
-            "name": "Indigenous Peoples' Day",
-             "start": "2025-10-13",
-             "end": "2025-10-13"
-           }
-           {
-            "name": "Veterans Day",
-             "start": "2025-11-10",
-             "end": "2025-11-11"
-           }
-           {
-            "name": "Thanksgiving",
-             "start": "2025-11-27",
-             "end": "2025-11-28"
-           }
-        ]
-    }
-    "spring": {
-        "start": "2026-02-02",
-        "end": "2026-05-12",
-        "breaks": [
-           {
-             "name": "Presidents' Day",
-             "start": "2026-02-16",
-             "end": "2026-02-16"
-           } 
-           {
-             "name": "Spring Break",
-             "start": "2026-03-23",
-             "end": "2026-03-27"
-           } 
-           {
-             "name": "Patriots' Day",
-             "start": "2026-04-20",
-             "end": "2026-04-20"
-           } 
-        ]
-    }
-}
 "University of California--Los Angeles": {
      "fall": {
         "start": "2025-09-25",
@@ -157,6 +79,475 @@
              "name": "Spring Break",
              "start": "2026-03-08",
              "end": "2026-03-15"
+           } 
+        ]
+    }
+"Brown University": {
+     "fall": {
+        "start": "2025-08-28",
+        "end": "2025-12-20",
+        "breaks": [
+           {
+             "name": "Indigenous Peoples' Day",
+             "start": "2025-10-13",
+             "end": "2025-10-13"
+           } 
+           {
+            "name": "Thanksgiving",
+             "start": "2025-11-26",
+             "end": "2025-11-30"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-01-21",
+        "end": "2026-05-15",
+        "breaks": [
+           {
+             "name": "Long Weekend",
+             "start": "2026-02-14",
+             "end": "2026-02-17"
+           } 
+           {
+             "name": "Spring Break",
+             "start": "2026-03-21",
+             "end": "2026-03-29"
+           } 
+        ]
+    }
+"Cornell University": {
+     "fall": {
+        "start": "2025-08-25",
+        "end": "2025-12-08",
+        "breaks": [
+           {
+             "name": "Labor Day",
+             "start": "2025-09-01",
+             "end": "2025-09-01"
+           } 
+           {
+            "name": "Fall Break",
+             "start": "2025-10-11",
+             "end": "2025-10-14"
+           }
+            {
+             "name": "Indigenous Peoples' Day",
+             "start": "2025-10-13",
+             "end": "2025-10-13"
+           } 
+           {
+            "name": "Thanksgiving",
+             "start": "2025-11-26",
+             "end": "2025-11-30"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-01-20",
+        "end": "2026-05-06",
+        "breaks": [
+           {
+             "name": "February Break",
+             "start": "2026-02-15",
+             "end": "2026-02-19"
+           } 
+           {
+             "name": "Spring Break",
+             "start": "2026-03-29",
+             "end": "2026-04-07"
+           } 
+        ]
+    }
+"Georgia Institute of Technology": {
+    "fall": {
+        "start": "2025-08-18",
+        "end": "2025-12-11",
+        "breaks": [
+            {
+             "name": "Labor Day",
+             "start": "2025-09-01",
+             "end": "2025-09-01"
+           } 
+            {
+             "name": "Fall Break",
+             "start": "2025-10-06",
+             "end": "2025-10-07"
+           } 
+           {
+            "name": "Thanksgiving",
+             "start": "2025-11-26",
+             "end": "2025-11-30"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-01-06",
+        "end": "2026-05-01",
+        "breaks": [
+           {
+             "name": "MLK Day",
+             "start": "2026-01-19",
+             "end": "2026-01-20"
+           } 
+           {
+             "name": "Spring Break",
+             "start": "2026-03-17",
+             "end": "2026-03-21"
+           } 
+        ]
+    }
+"Stanford University": {
+     "fall": {
+        "start": "2025-09-16",
+        "end": "2025-12-12",
+        "breaks": [
+           {
+             "name": "Democracy Day",
+             "start": "2025-11-04",
+             "end": "2025-11-04"
+           } 
+           {
+            "name": "Thanksgiving",
+             "start": "2025-11-24",
+             "end": "2025-11-28"
+           }
+        ]
+    }
+     "winter": {
+        "start": "2026-01-05",
+        "end": "2026-03-20",
+        "breaks": [
+           {
+             "name": "MLK Day",
+             "start": "2026-01-19",
+             "end": "2026-01-19"
+           } 
+           {
+            "name": "Presidents' Day",
+             "start": "2026-02-16",
+             "end": "2025-02-16"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-03-30",
+        "end": "2026-06-10",
+        "breaks": [
+           {
+             "name": "Memorial Day",
+             "start": "2026-05-25",
+             "end": "2026-05-25"
+           } 
+        ]
+    }
+"SUNY Canton": {
+      "fall": {
+        "start": "2025-08-21",
+        "end": "2025-12-12",
+        "breaks": [
+           {
+             "name": "Fall Break",
+             "start": "2025-10-09",
+             "end": "2025-10-10"
+           } 
+           {
+            "name": "Thanksgiving",
+             "start": "2025-11-22",
+             "end": "2025-11-30"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-01-21",
+        "end": "2026-05-08",
+        "breaks": [
+           {
+             "name": "Spring Break",
+             "start": "2026-03-12",
+             "end": "2026-03-21"
+           } 
+        ]
+    }
+"University of California: San Diego": {
+      "fall": {
+        "start": "2025-09-22",
+        "end": "2025-12-13",
+        "breaks": [
+           {
+            "name": "Veterans Day",
+             "start": "2025-11-11",
+             "end": "2025-11-11"
+           }
+           {
+            "name": "Thanksgiving",
+             "start": "2025-11-27",
+             "end": "2025-11-28"
+           }
+        ]
+    }
+    "winter": {
+        "start": "2026-01-02",
+        "end": "2026-03-21",
+        "breaks": [
+           {
+            "name": "MLK Day",
+             "start": "2026-01-19",
+             "end": "2026-01-19"
+           }
+           {
+            "name": "Presidents' Day",
+             "start": "2026-02-16",
+             "end": "2026-02-16"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-03-25",
+        "end": "2026-06-12",
+        "breaks": [
+            {
+             "name": "Cesar Chavez Holiday",
+             "start": "2026-03-27",
+             "end": "2026-03-27"
+           } 
+           {
+             "name": "Memorial Day",
+             "start": "2026-05-25",
+             "end": "2026-05-25"
+           } 
+        ]
+    }
+}
+"University of Florida": {
+     "fall": {
+        "start": "2025-08-21",
+        "end": "2025-12-12",
+        "breaks": [
+           {
+             "name": "Labor Day",
+             "start": "2025-09-01",
+             "end": "2025-09-01"
+           } 
+           {
+            "name": "Veterans Day",
+             "start": "2025-11-11",
+             "end": "2025-11-11"
+           }
+            {
+            "name": "Thanksgiving",
+             "start": "2025-11-23",
+             "end": "2025-11-30"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-01-12",
+        "end": "2026-05-01",
+        "breaks": [
+           {
+             "name": "MLK Day",
+             "start": "2026-01-19",
+             "end": "2026-01-19"
+           } 
+           {
+             "name": "Spring Break",
+             "start": "2026-03-14",
+             "end": "2026-03-21"
+           } 
+        ]
+    }
+"University of Illinois Urbana-Champaign": {
+     "fall": {
+        "start": "2025-08-25",
+        "end": "2025-12-18",
+        "breaks": [
+           {
+             "name": "Labor Day",
+             "start": "2025-09-01",
+             "end": "2025-09-01"
+           } 
+            {
+            "name": "Thanksgiving",
+             "start": "2025-11-22",
+             "end": "2025-11-30"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-01-21",
+        "end": "2026-05-15",
+        "breaks": [
+           {
+             "name": "Spring Break",
+             "start": "2026-03-15",
+             "end": "2026-03-23"
+           } 
+        ]
+    }
+"University of Michigan": {
+     "fall": {
+        "start": "2025-08-25",
+        "end": "2025-12-17",
+        "breaks": [
+           {
+             "name": "Labor Day",
+             "start": "2025-09-01",
+             "end": "2025-09-01"
+           } 
+           {
+            "name": "Fall Break",
+             "start": "2025-10-13",
+             "end": "2025-10-14"
+           }
+            {
+            "name": "Thanksgiving",
+             "start": "2025-11-26",
+             "end": "2025-11-28"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-01-07",
+        "end": "2026-04-30",
+        "breaks": [
+           {
+             "name": "MLK Day",
+             "start": "2026-01-19",
+             "end": "2026-01-19"
+           } 
+           {
+             "name": "Spring Break",
+             "start": "2026-02-28",
+             "end": "2026-03-09"
+           } 
+        ]
+    }
+"University of Virginia": {
+     "fall": {
+        "start": "2025-08-26",
+        "end": "2025-12-19",
+        "breaks": [
+           {
+             "name": "Labor Day",
+             "start": "2025-09-01",
+             "end": "2025-09-01"
+           } 
+           {
+            "name": "Fall Break",
+             "start": "2025-11-11",
+             "end": "2025-11-14"
+           }
+            {
+             "name": "Election Day",
+             "start": "2025-11-04",
+             "end": "2025-11-04"
+           } 
+            {
+            "name": "Thanksgiving",
+             "start": "2025-11-26",
+             "end": "2025-11-30"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-01-12",
+        "end": "2026-05-08",
+        "breaks": [
+           {
+             "name": "MLK Day",
+             "start": "2026-01-19",
+             "end": "2026-01-19"
+           } 
+           {
+             "name": "Spring Break",
+             "start": "2026-02-28",
+             "end": "2026-03-08"
+           } 
+        ]
+    }
+"Tuftfs University": {
+      "fall": {
+        "start": "2025-09-02",
+        "end": "2025-12-18",
+        "breaks": [
+           {
+             "name": "Indigenous Peoples' Day",
+             "start": "2025-10-13",
+             "end": "2025-10-13"
+           } 
+            {
+             "name": "Veterans Day",
+             "start": "2025-11-11",
+             "end": "2025-11-11"
+           } 
+            {
+            "name": "Thanksgiving",
+             "start": "2025-11-27",
+             "end": "2025-11-28"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-01-14",
+        "end": "2026-05-08",
+        "breaks": [
+            {
+             "name": "MLK Day",
+             "start": "2026-01-19",
+             "end": "2026-01-19"
+           } 
+             {
+            "name": "Presidents' Day",
+             "start": "2026-02-16",
+             "end": "2026-02-16"
+           }
+           {
+             "name": "Spring Break",
+             "start": "2026-03-14",
+             "end": "2026-03-23"
+           } 
+        ]
+    }
+"Villanova University": {
+     "fall": {
+        "start": "2025-08-25",
+        "end": "2025-12-19",
+        "breaks": [
+           {
+             "name": "Labor Day",
+             "start": "2025-09-01",
+             "end": "2025-09-01"
+           } 
+            {
+             "name": "Fall Break",
+             "start": "2025-10-13",
+             "end": "2025-10-20"
+           } 
+            {
+            "name": "Thanksgiving",
+             "start": "2025-11-25",
+             "end": "2025-12-01"
+           }
+        ]
+    }
+    "spring": {
+        "start": "2026-01-12",
+        "end": "2026-05-12",
+        "breaks": [
+            {
+             "name": "MLK Day",
+             "start": "2026-01-19",
+             "end": "2026-01-19"
+           } 
+             {
+            "name": "Mid-Term Break",
+             "start": "2026-03-02",
+             "end": "2026-03-09"
+           }
+           {
+             "name": "Easter Recess",
+             "start": "2026-04-01",
+             "end": "2026-04-07"
            } 
         ]
     }
